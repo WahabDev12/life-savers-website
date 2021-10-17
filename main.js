a80a9628c10aa29d2d74e5349a91d1407a21c416
Abdul Wahab Abass <nerdyshinobi12@gmail.com>
2021-10-17 15:29:55 +0000

    minor changes

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ const humpHausa = new Audio("/audio/hump-hausa.mp4");
 const stopHausa = new Audio("/audio/no-stop-hausa.mp4");
 const stopGa = new Audio("/audio/no-stop-ga.mp4");
 const looseGa = new Audio("/audio/loose-ga.mp4");
-
+const looseHausa = new Audio("/audio/loose-gravel-hausa.mp4");
+const roadWorkHausa = new Audio("/audio/road-work-hausa.mp4");
+const roadWorkGa = new Audio("/audio/road-work-ga.ogg");
+const unevenTwi = new Audio("/audio/uneven-twi.ogg");
+const noParkTwi = new Audio("/audio/no-park-twi.ogg");
+const humpTwi = new Audio("/audio/hump-twi.ogg");
+const noStopTwi = new Audio("/audio/no-stopping-twi.ogg");
+const looseTwi = new Audio("/audio/loose-gravels-twi.ogg");
 
 
 // UNEVEN ROAD
@@ -22,6 +29,9 @@ const changeLanguage = (selected) =>{
     }
     else if(selected === "Hausa"){
         hausaUneven.play()
+    }
+    else if(selected === "Twi"){
+        unevenTwi.play()
     }
     else if(selected === "Select your language"){
         alert("Please select a language 🙏🏼")
@@ -39,6 +49,9 @@ const changeLanguage_2 = (selected) => {
     if(selected === "Hausa"){
         noparkHausa.play()
     } 
+    else if(selected === "Twi"){
+        noParkTwi.play()
+    }
     else if(selected === "Select your language"){
         alert("Please select a language 🙏🏼")
     }  
@@ -54,6 +67,9 @@ const changeLanguage_3 = (selected)=>{
     if(selected === "Hausa"){
         humpHausa.play()
     } 
+    else if(selected === "Twi"){
+        humpTwi.play();
+    }
     else if(selected === "Select your language"){
         alert("Please select a language 🙏🏼")
     }   
@@ -68,6 +84,9 @@ const changeLanguage_4 = (selected)=>{
     if(selected === "Hausa"){
         stopHausa.play()
     }
+    else if(selected === "Twi"){
+        noStopTwi.play()
+    }
     else if(selected === "Select your language"){
         alert("Please select a language 🙏🏼")
     }    
@@ -81,11 +100,27 @@ const changeLanguage_5 = (selected)=>{
         looseGa.play()
     }
     if(selected === "Hausa"){
-        stopHausa.play()
-    }   
+        looseHausa.play()
+    }  
+    else if(selected === "Twi"){
+        looseTwi.play()
+    } 
     else if(selected === "Select your language"){
         alert("Please select a language 🙏🏼")
     } 
+}
+
+const changeLanguage_6 = (selected)=>{
+    var selected = option.value;
+    if(selected === "Ga"){
+        roadWorkGa.play()
+    }
+    if(selected === "Hausa"){
+        roadWorkHausa.play()
+    }   
+    else if(selected === "Select your language"){
+        alert("Please select a language 🙏🏼")
+    }    
 }
 
 
